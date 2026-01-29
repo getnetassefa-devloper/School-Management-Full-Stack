8ba5b8cf867eb956e5b80df36d57f004285e5faf
Getinet <chrisLover531@gmail.com>
2026-01-29 01:49:59 -0800

    Deleting API is built and the schema vs database conflict fixed with migrate

--- Controller/studentController.js
+++ Controller/studentController.js
@@ -14,7 +14,7 @@ export const getAllStudents = async (req, res) => {
 };
 export const getClassStudents = async (req, res) => {};
 export const addStudent = async (req, res) => {
-  const { fullName,gender, password,email } = req.body;
+  const { fullName, gender, password, email } = req.body;
   try {
     const userExists = await prisma.user.findUnique({
       where: { email },
@@ -34,10 +34,40 @@ export const addStudent = async (req, res) => {
         role: "STUDENT",
       },
     });
-    res.status(200) .json({ data: newStudent, success: true, message: "New Student Registered", });
+    res.status(200).json({
+      data: newStudent,
+      success: true,
+      message: "New Student Registered",
+    });
   } catch (err) {
-    res.status(500).json({ success: false, error: { message: `Your error---->  ${err}` } });
+    res
+      .status(500)
+      .json({ success: false, error: { message: `Your error---->  ${err}` } });
   }
 };
 export const editStudent = async (req, res) => {};
-export const deleteStudent = async (req, res) => {};
+export const deleteStudent = async (req, res) => {
+  const { studId } = req.params;
+  // Check now if the student id really exists
+  try {
+    const studExist = await prisma.user.findUnique({
+      where: { id: studId },
+      include: { student: true },
+    });
+    if (!studExist || studExist.role !=="STUDENT") {
+      console.log("Student does not exist")
+      return res
+        .status(404)
+        .json({ success: false, message: "Sudent not found" });
+    }
+    await prisma.user.delete({
+      where: { id: studId },
+    });
+    res
+      .status(200)
+      .json({ success: true, message: `Student ${studExist.fullName} is deleted ` });
+  } catch (err) {
+    console.log("Error in deletein---->",err)
+    res.status(500).json({ success: false, error: { message: `${err}` } });
+  }
+};
