04c0f249c28a45f8ce01108d9d80a25ff97d7aa6
Getinet Assefa <getnetassefa1122@gmail.com>
2026-02-05 05:28:50 -0800

    Nav Bar that is rendered dynamically for diffrent roles is built.It is successfull for ADMIN role.

--- src/app/admin/(dashboard)/layout.jsx
+++ src/app/admin/(dashboard)/layout.jsx
@@ -1,6 +1,7 @@
 import AdminNavigationBar from "@/components/admin/NavBar";
 // import AdminSideBar from '@/components/admin/SideBar'
 import UserSidebar from "@/components/app-sidebar";
+import UserNavbar from '@/components/app-navbar'
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
 export default function DashboardLayout({ children }) {
@@ -14,7 +15,7 @@ export default function DashboardLayout({ children }) {
         <SidebarTrigger />
         {/* The folloing is the navigation area for the main content page. */}
         <div className="flex flex-col flex-1">
-          <AdminNavigationBar />
+          <UserNavbar userRole={userRole} />
           {/* <h1>Below,Below</h1> */}
           <main className="p-6">{children}</main>
         </div>
---
+++ src/components/app-navbar.jsx
@@ -0,0 +1,123 @@
+"use client";
+import * as React from "react";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuContent,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+
+const navData = {
+  ADMIN: [
+    {
+      title: "School Managment System",
+      type: "dropdown",
+      content: [
+        {
+          title: "Student Managment",
+          href: "/students",
+          desc: "Manage Students",
+        },
+        {
+          title: "Teacher Managment",
+          href: "/teachers",
+          desc: "Manage Teachers",
+        },
+        {
+          title: "User Permisson",
+          href: "/admin/roles",
+          desc: "Manage User Role",
+        },
+      ],
+    },
+    { title: "Reports", type: "link", href: "/admin/reports" },
+  ],
+  STUDENT: [
+    {
+      title: "My Acadamic",
+      type: "dropdown",
+      content: [
+        {
+          title: "My Grade",
+          href: "/student/grade",
+          desc: "View my Grade report",
+        },
+        {
+          title: "Exam Schedule",
+          href: "/student/exam/schedule",
+          desc: "Exams Schedule ",
+        },
+      ],
+    },
+  ],
+  TEACHER: [],
+};
+
+export default function UserNavbar({ userRole = "ADMIN" }) {
+  const activeUser = navData[userRole] || [];
+  if (activeUser.length == 0) {
+    // console.log('No Role Detected')
+    return null;
+  }
+
+  return (
+    <div className="flex items-center justify-between px-6 py-4 border-b bg-white">
+      {/* Brand / Logo */}
+      <div className="font-bold text-xl tracking-tighter">
+        SATA<span className="text-blue-600"> College</span>
+      </div>
+
+      {/* Daynamic Navigation Based on the user Role */}
+      <NavigationMenu>
+        <NavigationMenuList>
+          {activeUser.map((item, index) =>
+            item.type === "dropdown" ? (
+              
+                <NavigationMenuItem key={item.title}>
+                  <NavigationMenuTrigger>{item.title}</NavigationMenuTrigger>
+                  <NavigationMenuContent>
+                    <ul className="grid w-[400px] gap-3 p-4">
+                      {item.content.map((subItem) => (
+                        <li key={subItem.title}>
+                          <NavigationMenuLink asChild>
+                            <Link
+                              href={subItem.href}
+                              className={cn(
+                                "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+                              )}
+                            >
+                              <div className="text-sm font-medium leading-none">
+                                {subItem.title}
+                              </div>
+                              <p>{subItem.desc}</p>
+                            </Link>
+                          </NavigationMenuLink>
+                        </li>
+                      ))}
+                    </ul>
+                  </NavigationMenuContent>
+                </NavigationMenuItem>
+              
+            ) : (
+              <NavigationMenuItem key={item.title}>
+                <NavigationMenuLink asChild>
+                  <Link href={item.href} className={navigationMenuTriggerStyle}>
+                    <div className="text-sm font-medium leading-none">
+                      {item.title}
+                    </div>
+                    <p>{item.desc}</p>
+                  </Link>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+            ),
+          )}
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
+  );
+}
